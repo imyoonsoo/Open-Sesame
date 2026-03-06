c336926cc8f0e8774cc69bf0be0e8cd4c5b5242a
douk9909 <douk9909@gmail.com>
2026-03-06 12:46:51 +0900

    feat: ListPage UI 및 네비게이션, 정렬 기능 추가

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,10 @@
-import Axios from 'axios';
+import Axios from 'axios'
 
+// OpenMind API 요청을 위한 axios 인스턴스 생성
+// baseURL을 설정해두면 이후 요청에서 주소를 반복 작성하지 않아도 됨
 const axios = Axios.create({
-  baseURL: 'https://openmind-api.vercel.app/23-4',
-});
+  baseURL: 'https://openmind-api.vercel.app/23-4', // 우리 팀(23기 4팀) API 기본 주소
+})
 
-export default axios;
+// 다른 파일에서 사용할 수 있도록 export
+export default axios
