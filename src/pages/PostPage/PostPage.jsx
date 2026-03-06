226c70ce75a3090923f467571bf21d3de74b33b2
imyoonsoo <bliss38soo@gmail.com>
2026-03-06 09:15:47 +0900

    refactor: convert to react components

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,9 +1,41 @@
+import '../PostPage/PostPage.css';
 import PostHeader from './components/PostHeader';
+import Profile from '../../assets/images/Profile.png';
+import { useState, useEffect } from 'react';
 
 function PostPage() {
+  const [data, setData] = useState({
+    name: '아초는 고양이',
+    profile: Profile,
+  });
+
+  const [isToast, setIsToast] = useState(false); // 토스트 기본 설정 -> false
+
+  const CopyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setIsToast(true);
+    setTimeout(() => setIsToast(false), 5000);
+  };
+
+  const ShareKakao = () => {
+    /* 구현 중 */
+    alert('카카오톡 공유!');
+  };
+
+  const ShareFacebook = () => {
+    /* 구현 중 */
+    alert('페이스북 공유!');
+  };
+
   return (
     <>
-      <PostHeader />
+      <PostHeader
+        {...data}
+        onClick_Link={CopyLink}
+        onClick_Kakao={ShareKakao}
+        onClick_Facebook={ShareFacebook}
+      />
+      {isToast && <div className="toast-msg">URL이 복사되었습니다!</div>}
     </>
   );
 }
--- src/pages/PostPage/components/PostHeader.jsx
+++ src/pages/PostPage/components/PostHeader.jsx
@@ -1,42 +1,55 @@
 import '../components/PostHeader.css';
 import OpenMindHeader from '../../../assets/images/OpenMindHeader.png';
 import OpenMindLogo from '../../../assets/images/OpenMindLogo.svg';
-import Profile from '../../../assets/images/Profile.png';
 import LinkVector from '../../../assets/images/Link.svg';
 import KakaoVector from '../../../assets/images/Kakaotalk.svg';
 import FacebackVector from '../../../assets/images/Facebook.svg';
+import { useState, useEffect } from 'react';
 
-function PostHeader() {
-  const Name = '아초는 고양이';
+/* 코드 구조가 반복되어 ShareButton 추가 */
+function ShareButton({ className, icon, alt, onClick }) {
+  return (
+    <>
+      <button className={className} onClick={onClick}>
+        <img src={icon} alt={alt} />
+      </button>
+    </>
+  );
+}
+
+function PostHeader({
+  name,
+  profile,
+  onClick_Link,
+  onClick_Kakao,
+  onClick_Facebook,
+}) {
   return (
     <>
       <div id="postpage-header">
         <img className="post-header" src={OpenMindHeader} alt="OpenMind 헤더" />
         <img className="post-logo" src={OpenMindLogo} alt="OpenMind 로고" />
-        <img className="post-profile" src={Profile} alt="OpenMind 프로필" />
-        <p className="post-name">{Name}</p>
-        <div className="post-icons">
-          <button className="post-linkBtn">
-            <img
-              className="post-linkVector"
-              src={LinkVector}
-              alt="링크 아이콘"
-            />
-          </button>
-          <button className="post-kakaoBtn">
-            <img
-              className="post-kakaoVector"
-              src={KakaoVector}
-              alt="카카오 아이콘"
-            />
-          </button>
-          <button className="post-facebookBtn">
-            <img
-              className="post-facebookVector"
-              src={FacebackVector}
-              alt="페이스북 아이콘"
-            />
-          </button>
+        <img className="post-profile" src={profile} alt="OpenMind 프로필" />
+        <p className="post-name">{name}</p>
+        <div className="post-share">
+          <ShareButton
+            className="post-linkBtn"
+            icon={LinkVector}
+            alt="링크"
+            onClick={onClick_Link}
+          />
+          <ShareButton
+            className="post-kakaoBtn"
+            icon={KakaoVector}
+            alt="카카오"
+            onClick={onClick_Kakao}
+          />
+          <ShareButton
+            className="post-facebookBtn"
+            icon={FacebackVector}
+            alt="페이스북"
+            onClick={onClick_Facebook}
+          />
         </div>
       </div>
     </>
