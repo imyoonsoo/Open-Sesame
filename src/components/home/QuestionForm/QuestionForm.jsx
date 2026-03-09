0a56d593a64d4bb53967cb2027c6fa834ae847a3
chahyunlee <chahyun023@gmail.com>
2026-03-09 11:51:31 +0900

    feat: InputField 공통 컴포넌트 분리

--- src/components/home/QuestionForm/QuestionForm.jsx
+++ src/components/home/QuestionForm/QuestionForm.jsx
@@ -24,19 +24,12 @@
 import './QuestionForm.css';
 import { Link } from 'react-router-dom';
 import nameicon from '@/assets/images/img-input-name.png';
+import InputField from '@/components/common/InputField/InputField';
 
 function QuestionForm() {
   return (
     <div className="question-card">
-      <div className="input-wrapper">
-        <img src={nameicon} alt="" className="input-icon" />
-
-        <input
-          type="text"
-          placeholder="이름을 입력하세요"
-          className="name-input"
-        />
-      </div>
+      <InputField placeholder="이름을 입력하세요" icon={nameicon} />
 
       <Link to="/list" className="submit-button">
         질문 받기
