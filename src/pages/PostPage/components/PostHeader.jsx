fd76b49712d5d287fbce5091d6d0a36761b3bd13
imyoonsoo <bliss38soo@gmail.com>
2026-03-10 05:03:11 +0900

    fix: 열려라참깨 로고, 파피콘 적용하여 반응형 수정 완료

--- src/pages/PostPage/components/PostHeader.jsx
+++ src/pages/PostPage/components/PostHeader.jsx
@@ -1,9 +1,12 @@
 import '../components/PostHeader.css';
-import OpenMindHeader from '../../../assets/images/OpenMindHeader.png';
-import OpenMindLogo from '../../../assets/images/OpenMindLogo.svg';
 import linkVector from '../../../assets/images/Link.svg';
 import KakaoVector from '../../../assets/images/Kakaotalk.svg';
 import facebookVector from '../../../assets/images/Facebook.svg';
+
+/* 오픈참깨 에셋 */
+import OpenSesame_bg from '../../../assets/images/OpenSesame/OpenSesame_bg.png';
+import OpenSesame_logo from '../../../assets/images/OpenSesame/OpenSesame_logo.svg';
+import OpenSesame_profile from '../../../assets/images/OpenSesame/OpenSesame_profile.svg';
 
 /* ShareButton 컴포넌트 (링크, 카카오, 페이스북) */
 function ShareButton({ className, icon, alt, onClick }) {
@@ -17,11 +20,15 @@ function ShareButton({ className, icon, alt, onClick }) {
 function PostHeader({ name, profile, linkIcon, kakaoIcon, facebookIcon }) {
   return (
     <div id="postpage-header">
-      <img className="post-header" src={OpenMindHeader} alt="OpenMind 헤더" />
+      <img className="post-header" src={OpenSesame_bg} alt="OpenMind 헤더" />
 
       <div className="post-content">
-        <img className="post-logo" src={OpenMindLogo} alt="OpenMind 로고" />
-        <img className="post-profile" src={profile} alt="OpenMind 프로필" />
+        <img className="post-logo" src={OpenSesame_logo} alt="OpenMind 로고" />
+        <img
+          className="post-profile"
+          src={OpenSesame_profile}
+          alt="OpenMind 프로필"
+        />
         <p className="post-name">{name}</p>
         <div className="post-share">
           <ShareButton
