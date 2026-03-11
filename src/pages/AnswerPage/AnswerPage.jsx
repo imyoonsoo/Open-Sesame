766663b5f72ba46830f9aa78356221dcac766912
Haning <100738360+Choiyuhyeon@users.noreply.github.com>
2026-03-11 12:15:48 +0900

    feat: answer페이지 컴포넌트 조립 완료

--- src/pages/AnswerPage/AnswerPage.jsx
+++ src/pages/AnswerPage/AnswerPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import FeedContainer from '@/components/answer/FeedContainer/FeedContainer';
+import PostHeader from '@/components/post/PostHeader/PostHeader';
 
 function AnswerPage() {
   return (
     <div>
+      <PostHeader />
       <FeedContainer />
     </div>
   );
