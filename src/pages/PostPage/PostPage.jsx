b977f7921dfdc24039218dd0c3199610658f5185
douk9909 <douk9909@gmail.com>
2026-03-12 15:44:47 +0900

    feat: 탈퇴하기 클릭 시 AlertModal 띄우기(AlertModal 재사용을 위해 props 추가)

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -4,6 +4,7 @@ import { useShare } from '@/hooks/useShare';
 import PostHeader from '@/components/post/PostHeader/PostHeader';
 import NoQuestion from '@/components/post/NoQuestion/NoQuestion';
 import QuestionButton from '@/components/post/QuestionButton/QuestionButton';
+import AlertModal from '@/components/common/AlertModal/AlertModal';
 
 function PostPage() {
   const [data, setData] = useState({
@@ -14,6 +15,7 @@ function PostPage() {
   const [showToast, setShowToast] = useState(false); // 토스트
   const [renderToast, setRenderToast] = useState(false); // 렌더링
   const { copyLink, shareKakao, shareFacebook } = useShare(setShowToast);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   useEffect(() => {
     if (showToast) {
@@ -26,6 +28,15 @@ function PostPage() {
     return () => clearTimeout(t);
   }, [showToast]);
 
+  const handleDeleteSubject = async () => {
+    try {
+      console.log('삭제 진행');
+      setIsDeleteModalOpen(false);
+    } catch (error) {
+      console.error('삭제 실패:', error);
+    }
+  };
+
   return (
     <>
       <div className="postpage-wrapper">
@@ -37,6 +48,20 @@ function PostPage() {
           onClickEditName={() => setIsEditModalOpen(true)}
           onClickDelete={() => setIsDeleteModalOpen(true)}
         />
+        {isDeleteModalOpen && (
+          <div
+            className="modal-overlay"
+            onClick={() => setIsDeleteModalOpen(false)}
+          >
+            <div onClick={(e) => e.stopPropagation()}>
+              <AlertModal
+                text="정말로 탈퇴하시겠습니까?"
+                onConfirm={handleDeleteSubject}
+                onCancel={() => setIsDeleteModalOpen(false)}
+              />
+            </div>
+          </div>
+        )}
         {renderToast && (
           <div className={`toast-msg ${showToast ? 'toast-in' : 'toast-out'}`}>
             URL이 복사되었습니다!
